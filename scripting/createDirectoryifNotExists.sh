e9c5c132d81e0154a17858104846d8d56cf340fa
roshan <45297269+bad-logic@users.noreply.github.com>
2024-11-05 17:19:29 -0600

    update

--- scripting/createDirectoryifNotExists.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-if [ -d hello  ]; then echo 'exist';else mkdir hello;fi
--- scripting/switchcase.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-MY_FRUIT="APPLE"
-
-case $MY_FRUIT in 
-    "APPLE") echo "APPLE";;
-    "MANGO") echo "MANGO";; 
-esac
